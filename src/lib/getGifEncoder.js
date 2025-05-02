36eeed48db84cf66143f07349d5bd8f7474a5d30
Rashid Khan <github.fiji@cspo.top>
2025-05-01 21:18:02 -0600

    Remove everything, switch to matr-server2

--- src/lib/getGifEncoder.js
+++
@@ -1,7 +0,0 @@
-import GIFEncoder from "gif-encoder-2";
-
-export default function getGifEncoder() {
-  const encoder = new GIFEncoder(64, 32, `neuquant`, true);
-  encoder.setPaletteSize(1)
-  return encoder
-}
