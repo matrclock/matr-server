08f2a45332bcda09780c66bd75bb5c1cd010e617
Rashid Khan <github.fiji@cspo.top>
2025-05-06 10:55:02 -0600

    Better clock?

--- src/sources/time.js
+++ src/sources/time.js
@@ -1,10 +1,12 @@
+import timezone from 'dayjs/plugin/timezone.js';
+import utc from 'dayjs/plugin/utc.js';
+import dayjs from 'dayjs';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export const time = () => {
-    const now = new Date();
-    now.setHours(now.getHours() - 6); // Adjust for UTC-6
-  
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-  
-    return `${hours}:${minutes}`;
+    const now = dayjs.utc().tz("America/Denver");
+    return now.format('HH:mm');
   }
   
