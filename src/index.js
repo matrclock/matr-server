0fb82b554b0b087c6f6fd96bc6123cb92191566c
Rashid Khan <github.fiji@cspo.top>
2025-05-13 16:07:46 -0600

    Xtrabyt probably blocks hetzner

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ import { on } from 'events';
 const apps = [
     {app: pixlet('sunrise_sunset'), dwell: 5}, 
     {app: weather}, 
-    {app: pixlet('xtrabyt', {key: "FUo73MLD"}), dwell: 3},
     {app: time}, 
     {app: coffeeOutside}
 ];
